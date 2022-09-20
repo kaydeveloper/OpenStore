f45ca6d488ff83d5bcaa199ac0de21439ced81b1
Kuldeep Singh <kayjoomla@gmail.com>
2022-09-20 11:18:01 +0530

    Added git config

---
+++ config.js
@@ -0,0 +1,4 @@
+export const projAddress = "";
+// Polygon TicketNFT2 0xDe51c3679D52b26d9d0EEF10FEdF19DCA31DdcDc
+export const nftmarketaddress = "0xB71fD4D9d4F16bfF86ecF3105b775E4b6Ebf4b08"
+export const nftaddress = "0x9f1Dfb6F812A4821B5dB030aa4EA785a584E4234"
